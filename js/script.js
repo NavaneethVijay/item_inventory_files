1c43117c75cc781b76f558eeb2eb5dad788f440a
NavaneethVijay <33292252+NavaneethVijay@users.noreply.github.com>
2017-11-01 22:57:03 +0530

    Final Project

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+        window.sr = ScrollReveal();
+        sr.reveal('.navbar', {
+          duration: 2000,
+          origin:'bottom'
+        });
+        sr.reveal('.showcase-left', {
+          duration: 1000,
+          origin:'left',
+          distance:'300px'
+        });
+        sr.reveal('.showcase-right', {
+          duration: 2000,
+          origin:'right',
+          distance:'300px'
+        });
+        sr.reveal('.showcase-btn', {
+          duration: 2000,
+          delay: 2000,
+          origin:'bottom'
+        });
+        sr.reveal('#testimonial div', {
+          duration: 2000,
+          origin:'bottom'
+        });
+        sr.reveal('.info-left', {
+          duration: 2000,
+          origin:'left',
+          distance:'300px',
+          viewFactor: 0.2
+        });
+		sr.reveal('.contact-left', {
+          duration: 1000,
+          origin:'top',
+          distance:'200px',
+          viewFactor: 0.2
+        });
+		sr.reveal('.contact-right', {
+          duration: 1000,
+          origin:'bottom',
+          distance:'200px',
+          viewFactor: 0.1
+        });
+		sr.reveal('.info-right', {
+          duration: 2000,
+          origin:'left',
+          distance:'300px',
+          viewFactor: 0.2
+        });
+		sr.reveal('.p-left', {
+          duration: 2000,
+          origin:'left',
+          distance:'300px',
+          viewFactor: 0.2
+        });
+        
+
+  
